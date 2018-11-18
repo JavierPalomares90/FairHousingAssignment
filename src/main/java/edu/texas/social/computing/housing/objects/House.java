0237f90de343d61defcb48dfcc64524bba1191f3
Beth Richardson <bethlrichardson@gmail.com>
2018-11-17 19:57:04 -0600

    Add equals and hashcode to House

--- src/main/java/edu/texas/social/computing/housing/objects/House.java
+++ src/main/java/edu/texas/social/computing/housing/objects/House.java
@@ -5,4 +5,22 @@ public class House {
     public House(int n) {
         index = n;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof House)) return false;
+        House other = (House) o;
+        return this.index == other.index;
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final long temp1 = Double.doubleToLongBits(this.index);
+        result = (result*PRIME) + (int)(temp1 ^ (temp1 >>> 32));
+        return result;
+    }
+
 }
